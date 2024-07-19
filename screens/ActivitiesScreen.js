2fb2c3aba005df09ee7497fe46a03f3614d25731
cold-flatwhite <canadasun2022@gmail.com>
2024-07-18 21:43:34 -0700

    added activities screen and use native navigator to show in the App.js

---
+++ screens/ActivitiesScreen.js
@@ -0,0 +1,13 @@
+import {Text, View, Button } from 'react-native'
+import React from 'react'
+
+const ActivitiesScreen = ({}) => {
+  return (
+    <View>
+      <Text>Activities</Text>
+    </View>
+  )
+}
+
+export default ActivitiesScreen
+
