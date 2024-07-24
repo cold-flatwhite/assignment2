4d8933dfb97c9c0af340a6057b5670b9e693fae7
cold-flatwhite <canadasun2022@gmail.com>
2024-07-24 14:24:48 -0700

    initialized the steup of firebase database, set up the @env environment variable, put secret info in .env file

---
+++ Firebase/firebaseSetup.js
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+import {
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+} from "@env";
+
+const firebaseConfig = {
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+export const database = getFirestore(app);
--- babel.config.js
+++ babel.config.js
@@ -3,4 +3,21 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
   };
+  plugins : [ 
+    [ 
+      'module:react-native-dotenv' , 
+      { 
+        envName : 'APP_ENV' , 
+        moduleName : '@env' , 
+        path : '.env' , 
+        blocklist : null , 
+        allowlist : null , 
+        blacklist : null ,  
+        whitelist : null ,  
+        safe : false , 
+        allowUndefined : true , 
+        verbose : false , 
+      } , 
+    ] , 
+  ];
 };
